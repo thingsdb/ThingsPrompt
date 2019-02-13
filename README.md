0ce1cbff079fcdb57e99e8315352854d24afbee3
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-02-13 22:05:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ThingsPrompt
+Shell client for ThingsDB
+
+
+## Installation
+Using pip:
+```shell
+pip insall thingsprompt
+```
+
+# Example usage:
+```shell
+thingsprompt -n localhost -u admin
+```
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='thingsprompt',
+    version='0.0.1',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    scripts=['bin/thingsprompt'],
+)
