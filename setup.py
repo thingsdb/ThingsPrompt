cf274d06e2a033c21879b6745121550371bd1ee4
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-02-23 22:44:45 +0100

    Added return `all` statements option

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='thingsprompt',
-    version='0.0.2',
+    version='0.0.3',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
