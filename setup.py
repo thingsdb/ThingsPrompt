267b5b51ec55b264ed30c96916e38cf8725540e3
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-05-09 15:26:06 +0200

    Add multiline support

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.0.3',
+    version='0.1.0',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
