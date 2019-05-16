035608afc46450791f503c09a12d44a2bfae27f6
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-05-16 13:57:06 +0200

    Update prompt for node/thingsdb scope

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.0',
+    version='0.1.1',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
