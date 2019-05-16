e3fddc883b19e5f86b3f19f7637a7f83633f211b
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-05-16 14:19:13 +0200

    Fix shebang

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.1',
+    version='0.1.2',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
