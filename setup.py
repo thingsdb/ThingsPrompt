fb04f34d7f581e0a3730a0f5427a376c5cdaf0b3
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-05-29 00:30:01 +0200

    Added shotduts for scopes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.3',
+    version='0.1.6',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
