8a39a809d9923de4890a6dc1e262ecee950db846
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-06-13 09:17:51 +0200

    Upd for new connector

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.7',
+    version='0.1.8',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
