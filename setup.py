29df33b32d04ac628496066ee1b9de77eac1060c
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-07-02 21:06:04 +0200

    Fixed shortcuts `node` and `thingsdb` scope

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.10',
+    version='0.1.11',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
