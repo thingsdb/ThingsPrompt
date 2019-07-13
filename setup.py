4eb090edb8f377acb45a2c298facc142d8bffda5
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-07-13 17:39:00 +0200

    Remove `deep` argument

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.12',
+    version='0.1.13',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
