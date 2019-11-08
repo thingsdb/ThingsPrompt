16832991790239ac635d40f2ecf2283761263fee
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-11-08 10:16:20 +0100

    Fix token auth

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.14',
+    version='0.1.15',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
