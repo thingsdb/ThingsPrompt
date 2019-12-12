f951560172c36f8bd8f13808170ac4e19d2a9724
Jeroen van der Heijden <jeroen@transceptor.technology>
2019-12-12 22:26:57 +0100

    Add SSL support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thingsprompt',
-    version='0.1.15',
+    version='0.1.16',
     author='Jeroen van der Heijden',
     author_email='jeroen@transceptor.technology',
     scripts=['bin/thingsprompt'],
