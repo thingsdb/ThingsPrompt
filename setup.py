c9c4f50b4c7442b164cd2c2407bb922080b63863
Jeroen van der Heijden <jeroen@cesbit.com>
2022-10-07 10:07:21 +0200

    Upd email

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Jeroen van der Heijden',
-    author_email='jeroen@transceptor.technology',
+    author_email='jeroen@cesbit.com',
     scripts=['bin/things-prompt'],
     license='GPLv3',
     classifiers=[
